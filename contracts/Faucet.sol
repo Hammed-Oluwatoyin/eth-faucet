d3a8f4f8ed43582484ecdab42d85da6238fde96a
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2022-09-07 12:46:54 +0100

    payable functions

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -4,4 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
     //storage Variables
     uint256 public funds = 1000;
+
+    receive() external payable {}
+    function addFunds() external payable  {}
 }
