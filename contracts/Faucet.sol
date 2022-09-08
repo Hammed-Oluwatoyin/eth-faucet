ff21fe8b1645f3c272a5bc4e25ca688d08a28633
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2022-09-08 12:48:23 +0100

    faucet funders

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,9 +2,28 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    //storage Variables
-    uint256 public funds = 1000;
+    address[] public funders;
 
     receive() external payable {}
-    function addFunds() external payable  {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+    }
 }
+
+// const instance = await Faucet.deployed()
+
+//instance.addFunds({value: "200000000000", from: accounts[0]})
+
+// instance.getAllFunders()
+
+//instance.getFunderAtIndex(0);
